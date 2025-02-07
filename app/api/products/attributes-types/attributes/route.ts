2eabb2b961e9ea4c88e05b6983f1e082a488fa42
RodrigoG <eduis.carranza123@gmail.com>
2025-02-06 21:36:41 -0500

    modificaciones en la api

---
+++ app/api/products/attributes-types/attributes/route.ts
@@ -0,0 +1,46 @@
+'use server'
+import AttributeService from "@/features/attributes/AttributeService";
+import AttributeTypesService from "@/features/attributes/AttributeTypesService";
+import { handleError } from "@/utils/serverUtils";
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    try {
+        // Busqueda de parametros
+        const searchParams = request.nextUrl.searchParams
+        const queryPage = searchParams.get('page')
+        const pagina = queryPage ? parseInt(queryPage) : 1
+
+        const queryitems = searchParams.get('limit')
+        const items = queryitems ? parseInt(queryitems) : 10
+
+        // Busqueda
+        const data = await AttributeService.getAll(pagina, items);
+        // Retornar la respuesta
+        return new Response(JSON.stringify(data), { 
+            status: 200, 
+            headers: { "Content-Type": "application/json" }
+        });
+    } catch (error) {
+        // Retornar un error en caso de que algo falle
+        return new Response(
+            JSON.stringify({ error: error instanceof Error ? error.message : "Unknown error occurred" }),
+            { 
+                status: 500, 
+                headers: { "Content-Type": "application/json" } 
+            }
+        );
+    }
+    
+}
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const data = await request.json();
+        const newData = await AttributeService.create(data);
+        return NextResponse.json(newData, { status: 201 });
+    } catch (error) {
+        return handleError(error)
+    }
+}
