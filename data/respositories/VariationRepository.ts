eee3d358c183b3d6056bb5b49bdd72eafd8c978e
RodrigoG <eduis.carranza123@gmail.com>
2025-02-14 23:00:10 -0500

    se modifico el repositorio de variaciones para adaptarlo a la nueva api

--- data/respositories/VariationRepository.ts
+++ data/respositories/VariationRepository.ts
@@ -1,4 +1,5 @@
 import createSupabaseClient from '@/utils/dbClient';
+import { makePagination } from '@/utils/serverUtils';
 import { SupabaseClient } from '@supabase/supabase-js';
 
 
@@ -10,21 +11,29 @@ export default new class VariationRepository {
         this.client = createSupabaseClient();
     }
 
-    // Obtener todas las variaciones
-    async getVariations(): Promise<Variation[]> {
+    // Obtener todas las variaciones de un producto
+    async getVariationsByProduct(page: number, itemsPerPage: number, productId: number): Promise<PaginatedResponse<Variation[]>> {
+        // Calcular los índices de paginación
+        const startIndex = (page - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage - 1;
+
         const { data, error } = await this.client
             .from('variaciones')
-            .select('*');
+            .select('*')
+            .eq('producto_id', productId)
+            .range(startIndex, endIndex);
 
         if (error) {
             console.error('Error fetching variations:', error);
             throw new Error('Unable to fetch variations');
         }
-        return data || [];
+        
+        return makePagination<Variation[]>(this.client, data, 'variaciones', page, itemsPerPage)
+
     }
 
     // Obtener una variación específica por su ID
-    async getVariation(id: number): Promise<Variation | null> {
+    async getVariation(id: number): Promise<DataResponse<Variation>> {
         const { data, error } = await this.client
             .from('variaciones')
             .select('*')
@@ -35,25 +44,16 @@ export default new class VariationRepository {
             console.error('Error fetching variation:', error);
             throw new Error('Unable to fetch variation');
         }
-        return data || null;
-    }
 
-    // Obtener variaciones por ID de producto
-    async getVariationsByProductId(productId: number): Promise<Variation[]> {
-        const { data, error } = await this.client
-            .from('variaciones')
-            .select('*')
-            .eq('producto_id', productId);
-
-        if (error) {
-            console.error('Error fetching variations by product ID:', error);
-            throw new Error('Unable to fetch variations by product ID');
+        // Lo envolvemos en un DataResponse
+        const res: DataResponse<Variation> = {
+            data: data || null,
         }
-        return data || [];
+        return res;
     }
 
     // Crear una nueva variación
-    async createVariation(variation: Partial<Variation>): Promise<Variation> {
+    async createVariation(variation: Partial<Variation>): Promise<DataResponse<Variation>> {
         const { data, error } = await this.client
             .from('variaciones')
             .insert(variation)
@@ -67,11 +67,15 @@ export default new class VariationRepository {
             console.error('No records found to create');
             throw new Error('No records found');
         }
-        return data[0]; 
+        // Lo envolvemos en un DataResponse
+        const res: DataResponse<Variation> = {
+            data: data[0] || null,
+        }
+        return res;
     }
 
     // Actualizar una variación existente
-    async updateVariation(id: number, updates: Partial<Variation>): Promise<Variation> {
+    async updateVariation(id: number, updates: Partial<Variation>): Promise<DataResponse<Variation>> {
         const { data, error } = await this.client
             .from('variaciones')
             .update(updates)
@@ -86,19 +90,29 @@ export default new class VariationRepository {
             console.error('No records found to update');
             throw new Error('No records found');
         }
-        return data[0]; 
+        // Lo envolvemos en un DataResponse
+        const res: DataResponse<Variation> = {
+            data: data[0] || null,
+        }
+        return res;
     }
 
     // Eliminar una variación por su ID
-    async deleteVariation(id: number): Promise<void> {
-        const { error } = await this.client
+    async deleteVariation(id: number): Promise<DataResponse<Variation>> {
+        const { data, error } = await this.client
             .from('variaciones')
             .delete()
-            .eq('id', id);
+            .eq('id', id)
+            .select();
 
         if (error) {
             console.error('Error deleting variation:', error);
             throw new Error('Unable to delete variation');
         }
+        // Lo envolvemos en un DataResponse
+        const res: DataResponse<Variation> = {
+            data: data[0] || null,
+        }
+        return res;
     }
 }
