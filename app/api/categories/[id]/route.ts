fc16ab932928a94cc4c694d91028b6446bf4188a
Rodrigo <eduis.carranza123@gmail.com>
2025-08-29 10:49:25 -0500

    correcion de la ruta categorias

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -48,7 +48,7 @@ export async function PUT(
 }
 
 export async function DELETE(
-    request: Request,
+    _request: Request,
     { params }: { params: { id: string } } // Asegúrate de que el parámetro sea 'id'
 ) {
     const id = params.id; // Obtén el id directamente
